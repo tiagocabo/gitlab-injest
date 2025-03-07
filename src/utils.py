dc88f199a06fbac6a34965331e680459aa2e5c24
tiago.cabo <tiago.cabo@olx.com>
2025-03-07 14:14:26 +0000

    restructure tests files

--- src/utils.py
+++ src/utils.py
@@ -1,42 +1,8 @@
-import requests
-import base64
 import logging
 
+from src.gitlab.gitlab_api import list_subfolder, read_repo_file
+
 logger = logging.getLogger(__name__)
-
-
-def list_subfolder(organization, repo_url, gitlab_token, folder="."):
-    url = f"https://{organization}/api/v4/projects/{repo_url}/repository/tree"
-    headers = {"PRIVATE-TOKEN": gitlab_token}
-    params = {"ref": "main", "path": folder}
-
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code == 200:
-        try:
-            files = response.json()
-            return [file["path"] for file in files]
-        except ValueError:
-            logger.info("Failed to parse JSON response.")
-            return []
-    elif response.status_code == 404:
-        logger.info("Folder not found.")
-        return []
-
-
-def read_repo_file(organization, repo_url, gitlab_token, file_path):
-    file_path = file_path.replace("/", "%2F")
-    url = f"https://{organization}/api/v4/projects/{repo_url}/repository/files/{file_path}"
-    params = {"ref": "main"}
-    headers = {"PRIVATE-TOKEN": gitlab_token}
-
-    response = requests.get(url, headers=headers, params=params)
-    file = response.json()
-    if "content" in file:
-        file_content = base64.b64decode(file["content"]).decode("utf-8")
-    else:
-        return None
-    return file_content
 
 
 def prepare_content(organization, file, repo_url, gitlab_token):
---
+++ tests/unit/utils/test_prepare_content.py
@@ -0,0 +1,55 @@
+from unittest.mock import patch
+from src.utils import prepare_content
+
+
+def test_prepare_content_python_file():
+    """Test when a Python file is processed successfully."""
+    with patch(
+        "src.gitlab.gitlab_api.read_repo_file", return_value="print('Hello, world!')"
+    ):
+        result = prepare_content(
+            "gitlab.example.com", "script.py", "my-repo", "fake_token"
+        )
+
+    expected_header = "=" * 50 + "\nscript.py\n" + "=" * 50
+    expected_content = f"\n{expected_header}\nprint('Hello, world!')"
+    assert result == expected_content
+
+
+def test_prepare_content_markdown_file():
+    """Test when a Markdown file is processed successfully."""
+    with patch(
+        "src.utils.gitlab.gitlab_api.read_repo_file",
+        return_value="# This is a README file.",
+    ):
+        result = prepare_content(
+            "gitlab.example.com", "README.md", "my-repo", "fake_token"
+        )
+
+    expected_header = "=" * 50 + "\nREADME.md\n" + "=" * 50
+    expected_content = f"\n{expected_header}\n# This is a README file."
+    assert result == expected_content
+
+
+def test_prepare_content_empty_python_file():
+    """Test when a Python file is empty."""
+    with patch("src.utils.gitlab.gitlab_api.read_repo_file", return_value=""):
+        result = prepare_content(
+            "gitlab.example.com", "empty_file.py", "my-repo", "fake_token"
+        )
+
+    expected_header = "=" * 50 + "\nempty_file.py\n" + "=" * 50
+    expected_content = f"\n{expected_header}\n"
+    assert result == expected_content
+
+
+def test_prepare_content_unsupported_file():
+    """Test when a file that is not .py or .md is provided."""
+    with patch(
+        "src.utils.gitlab.gitlab_api.read_repo_file", return_value="Some content"
+    ):
+        result = prepare_content(
+            "gitlab.example.com", "image.jpg", "my-repo", "fake_token"
+        )
+
+    assert result == ""  # The function should return an empty string
